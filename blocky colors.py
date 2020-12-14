7651f7e5c14d084b0fdf0ffcf27c20120a8fd5ca
Aashik J Krishnan <58915216+Aash-Gates@users.noreply.github.com>
2020-12-14 09:38:02 +0530

    09:37AM 14-DEC-2020

---
+++ blocky colors.py
@@ -0,0 +1,20 @@
+colors = None
+
+def lists_sort(my_list, type, reverse):
+  def try_float(s):
+    try:
+      return float(s)
+    except:
+      return 0
+  key_funcs = {
+    "NUMERIC": try_float,
+    "TEXT": str,
+    "IGNORE_CASE": lambda s: str(s).lower()
+  }
+  key_func = key_funcs[type]
+  list_cpy = list(my_list)
+  return sorted(list_cpy, key=key_func, reverse=reverse)
+
+
+colors = ['Violet', 'Red', 'Blue']
+print(lists_sort(colors, "TEXT", True))
